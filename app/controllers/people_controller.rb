34c820afeca0a7d6d23f0a97b3f691561f5a2619
Ross Andrews <randrews@ross-andrewss-macbook.local>
2009-05-19 13:45:19 -0500

    Made ajax from sc work

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,6 @@
 class PeopleController < ApplicationController
 
   def index
-    render :json => PeoplePresenter.new.to_hash
+    render :json => PersonPresenter.new.to_hash
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,11 @@
 class Person < ActiveRecord::Base
 
   def to_hash
-    attributes.merge :guid=>id, :type=>'Person'
+    attrs={}
+    attributes.merge(:guid=>id, :type=>'Person').each do |k,v|
+      attrs[k.to_s.camelize(:lower)]=v
+    end
+    attrs
   end
 
 end
