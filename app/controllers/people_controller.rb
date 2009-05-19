4a8a7de6b705da64f08f0fe2dbdbe0bf19757332
Ross Andrews <randrews@ross-andrewss-macbook.local>
2009-05-19 17:30:37 -0500

    Implemented update action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,16 @@ class PeopleController < ApplicationController
   def index
     render :json => PersonPresenter.new.to_hash
   end
+
+  def update
+    p=Person.find_by_id params[:id]
+    par=JSON::parse params[:person]
+    
+    p.first_name=par['firstName']
+    p.last_name=par['lastName']
+    p.salary=par['salary']
+    p.save
+
+    render :json=>p.to_hash
+  end
 end
