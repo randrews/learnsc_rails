828d749813d3e971bbbbe5a4016b1c5a64769e73
Ross Andrews <randrews@ross-andrewss-macbook.local>
2009-05-20 11:21:32 -0500

    Added create method

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,4 +15,23 @@ class PeopleController < ApplicationController
 
     render :json=>p.to_hash
   end
+
+  def create
+    # this is just SproutCore's temporary guid; we'll return the persistent, real guid if this is successful
+    @person_params = clean_params(JSON.parse(params[:person]))
+    person = Person.create!(@person_params)
+    render :json => person.to_hash
+  rescue ActiveRecord::RecordInvalid
+    render :text => "Unable to create person due to #{$!.message}", :status => :unprocessable_entity
+  end
+
+  private
+
+  def clean_params hash
+    clean={}
+    hash.slice('id','firstName','lastName','salary').each do |k,v|
+      clean[k.underscore]=v
+    end
+    clean
+  end
 end
